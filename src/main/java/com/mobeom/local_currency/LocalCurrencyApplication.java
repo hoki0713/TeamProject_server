7d716b4a6435b0d84aa54478c175f05a83f8e4b4
hoki0713 <hojeong0713@gmail.com>
2020-07-07 16:50:05 +0900

    server

---
+++ src/main/java/com/mobeom/local_currency/LocalCurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.mobeom.local_currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalCurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalCurrencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobeom/local_currency/LocalCurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobeom.local_currency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalCurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
