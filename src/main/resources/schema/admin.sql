9377b3f99a43b14189334d555b48adb9157aca37
황혜영 <62981058+hhy9@users.noreply.github.com>
2020-07-26 19:09:25 +0900

    sql update finish

--- src/main/resources/schema/admin.sql
+++ src/main/resources/schema/admin.sql
@@ -1,8 +1,9 @@
 CREATE TABLE admin
 (
-industry_code INT NOT NULL AUTO_INCREMENT,
+admin_id NOT NULL AUTO_INCREMENT,
+industry_code INT NULL ,
 amount_used INT NULL,
 city VARCHAR(20) NULL,
-store_type VARCHAR(10) NULL,
+classify_industry VARCHAR(20) NULL,
 PRIMARY KEY (industry_code)
 )default character set utf8 collate UTF8_GENERAL_CI;
--- src/main/resources/schema/voucher.sql
+++ src/main/resources/schema/voucher.sql
@@ -1,7 +1,7 @@
 CREATE TABLE voucher
 (
-voucher_no INT NOT NULL AUTO_INCREMENT,
-local_currency VARCHAR(10) NULL,
+voucher_id INT NOT NULL AUTO_INCREMENT,
+local_currency_id INT NULL,
 currency_type INT NULL,
 PRIMARY KEY (voucher_no)
 )default character set utf8 collate UTF8_GENERAL_CI;
