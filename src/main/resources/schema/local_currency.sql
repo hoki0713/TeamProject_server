c11d75f15d6c9b12a234d62d3da41c063c79ead3
hoki0713 <hojeong0713@gmail.com>
2020-07-28 12:32:52 +0900

    Add local_currency sql file

---
+++ src/main/resources/schema/local_currency.sql
@@ -0,0 +1,6 @@
+CREATE TABLE local_currency
+(
+    `local_currency_id`  INT            NOT NULL    AUTO_INCREMENT,
+    `use_local`          VARCHAR(10)    NULL,
+    PRIMARY KEY (local_currency_id)
+)default character set utf8 collate UTF8_GENERAL_CI;
