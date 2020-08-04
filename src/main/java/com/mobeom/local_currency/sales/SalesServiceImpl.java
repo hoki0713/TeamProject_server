d2e3bca9a0b6754dde5a9f81380592698725d93a
hoki0713 <hojeong0713@gmail.com>
2020-08-04 18:07:10 +0900

    Fix dummy generator and Complete connect PurchaseHistory client and server

--- src/main/java/com/mobeom/local_currency/sales/SalesServiceImpl.java
+++ src/main/java/com/mobeom/local_currency/sales/SalesServiceImpl.java
@@ -12,6 +12,10 @@ import java.util.Optional;
 interface SalesService {
 
     Optional<Map<Long, RequestedPurchaseHistoryVO>> getHistoryByUserId(long userId);
+
+    Optional<Sales> findVoucher(long voucherId);
+
+    Sales update(Sales selectedHistory);
 }
 
 @Service
@@ -23,5 +27,15 @@ public class SalesServiceImpl implements SalesService {
     public Optional<Map<Long, RequestedPurchaseHistoryVO>> getHistoryByUserId(long userId) {
         Map<Long, RequestedPurchaseHistoryVO> historyVOMap = salesRepository.findAllSalesRecordsByUserId(userId);
         return Optional.ofNullable(historyVOMap);
+    }
+
+    @Override
+    public Optional<Sales> findVoucher(long voucherId) {
+        return salesRepository.findById(voucherId);
+    }
+
+    @Override
+    public Sales update(Sales selectedHistory) {
+        return salesRepository.save(selectedHistory);
     }
 }
