63f3a2d774b778703b335e8deb9d4b396fea1df1
hoki0713 <hojeong0713@gmail.com>
2020-08-19 01:42:27 +0900

    Add code for user searching in admin userList component

--- src/main/java/com/mobeom/local_currency/user/UserRepositoryImpl.java
+++ src/main/java/com/mobeom/local_currency/user/UserRepositoryImpl.java
@@ -2,8 +2,12 @@ package com.mobeom.local_currency.user;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,6 +15,8 @@ interface CustomUserRepository {
     Optional<User> findByUserId(String userId);
     Optional<User> findByUserNameAndEmail(String name, String email);
     Optional<User> findByUserIdNameEmail(String userId, String name, String email);
+
+    List<User> findByUserName(String searchWord);
 }
 
 public class UserRepositoryImpl extends QuerydslRepositorySupport implements CustomUserRepository {
@@ -40,6 +46,14 @@ public class UserRepositoryImpl extends QuerydslRepositorySupport implements Cus
         QUser qUser = QUser.user;
         User findOne = queryFactory.selectFrom(qUser).where(qUser.userId.eq(userId), qUser.name.eq(name), qUser.email.eq(email)).fetchOne();
         return Optional.ofNullable(findOne);
+    }
+
+    @Override
+    public List<User> findByUserName(String searchWord) {
+        QUser qUser = QUser.user;
+        List<User> users =
+                queryFactory.selectFrom(qUser).where(qUser.name.like(searchWord)).fetch();
+        return users;
     }
 
 }
